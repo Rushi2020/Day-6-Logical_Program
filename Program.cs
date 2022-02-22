accf7b84a08e588b590ee6ac0556c9a7ae999f00
Rushi <189489rishi@gmail.com>
2022-02-22 09:45:27 +0530

    Added:Stop watch

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿//      Stop Watch 
+
+using System.Diagnostics;
+
+namespace Stop_watch
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.ReadLine();
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            Console.ReadLine();
+            stopwatch.Stop();
+            var elapsed_time = stopwatch.ElapsedMilliseconds;
+            Console.WriteLine(elapsed_time / 1000);
+
+
+        }
+    }
+}
+
+
+
+
+
+
+
