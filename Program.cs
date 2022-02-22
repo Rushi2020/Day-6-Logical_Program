2cf08a9875048766060e8c5708c0de4cb2faccae
Rushi <189489rishi@gmail.com>
2022-02-22 19:19:29 +0530

    Added:temprature conversion program

--- Program.cs
+++ Program.cs
@@ -1,23 +1,32 @@
-﻿// Reverse number
+﻿// Temprature Conversion
 using System;
 
-namespace everse_Number
+namespace Temp_Conversion
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine(" Enter value of num");
-            int num = Convert.ToInt32(Console.ReadLine());
-            int  reminder  , reverse = 0;
+            Console.WriteLine("Select Temprature Conversion Method");
+            Console.WriteLine("Choose 1:Celsius to Fahrenheit   2: Fahrenheit to Celsius ");
+            int option = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Put Temp. Value To convert");
+            int temp = Convert.ToInt32(Console.ReadLine());
 
-            while(num>0){
-                reminder = num % 10;
-                reverse = (reverse * 10) + reminder;
-                num = num / 10;
+            switch (option)
+            {
+                case 1:
+                    double celsius_To_Fahrenheit = (temp * 9 / 5) + 32;
+                    Console.WriteLine("Temperature in Fahrenheit: " + celsius_To_Fahrenheit);
+                    break;
+                case 2:
+                    double fahrenheit_To_Celsius = (temp - 32) * 5 / 9;
+                    Console.WriteLine("Temperature in Celsius: " + fahrenheit_To_Celsius);
+                    break;
+                default:
+                    Console.WriteLine("Invalid input");
+                    break;
             }
-            Console.WriteLine(reverse);
-           // Console.ReadLine();
         }
     }
 
